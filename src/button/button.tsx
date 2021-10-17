d47565311d4ff3529c938e7cc48758499674ec68
MadCcc <1075746765@qq.com>
2021-10-17 19:10:20 +0800

    feat: add styles for button

--- src/button/button.tsx
+++ src/button/button.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import './style';
 
 export type ButtonProps = {
   children?: React.ReactNode;
 };
 
 const Button: React.FC<ButtonProps> = ({ children }: ButtonProps) => {
-  return <button type="button">{children}</button>;
+  return (
+    <button type="button" className="ui-starter-button">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
